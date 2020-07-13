294fe175fb403e16ada44e50c52808d13797a4bd
doped-semiconductor <getsreya@gmail.com>
2020-07-13 14:00:46 +0530

    transit2

--- js/index.js
+++ js/index.js
@@ -1,11 +1,16 @@
-<html>
-
-    <head>
-        <title>Sreya Goswami</title>
-    </head>
-
-    <body>
-        
-    </body>
-
-</html>
+window.onload = () => {
+    var i = document.getElementById('i')
+    var b = document.getElementById('p')
+    b.addEventListener('click', (ev) => {
+        if(window.innerHeight > window.innerWidth){
+            i.setAttribute('style','transform: scale(14) translateY(14%) translateX(-5%);')
+        }
+        else{
+            i.setAttribute('style','transform: scale(10) translateX(-20%) translateY(15%);')
+        }
+        i.addEventListener('transitionend', () => {
+            window.open('projects.html','_self')
+        })
+                   
+    })
+}
